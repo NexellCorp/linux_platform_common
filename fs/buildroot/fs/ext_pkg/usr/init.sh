5ff5a31176b000f79c1e7c91f5ff5d833a0241b6
kjlee <kjlee@nexell.co.kr>
2016-09-23 22:37:47 +0900

    [PATCH] Corona  - Modify corona package
    Former-commit-id: 6633e8822170d69c436b9157c13e4d2aa4bab6b0

--- fs/buildroot/fs/ext_pkg/usr/init.sh
+++ fs/buildroot/fs/ext_pkg/usr/init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export PATH=$PATH:/mnt/mmc
+
 insmod /root/wlan.ko ifname=wlan0 if2name=p2p0
 chmod +x /usr/sbin/*
 chmod +x /usr/bin/*
@@ -13,19 +15,13 @@ busybox ifconfig p2p0 up
 #p2p_supplicant -s -B -Dnl80211 -ip2p0 -c /usr/etc/p2p_supplicant.conf
 
 # hostap
-
-# supplicant mode
 #busybox ifconfig wlan0 up
 #wpa_supplicant -s -B -Dnl80211 -iwlan0 -c /usr/etc/wpa_supplicant.conf
-#udhcpc -iwlan0 &
 
 # cas
-mkdir -p /usr/etc/ca
 mkdir -p /data
 ln -s /usr/etc/ca /data/ca
 chmod 777 /data/ca
 
-echo 18d1 > /sys/class/android_usb/android0/idVendor
-echo 4EE7 > /sys/class/android_usb/android0/idProduct
-echo adb > /sys/class/android_usb/android0/functions
-echo 1 > /sys/class/android_usb/android0/enable
+uxtest&
+
--- fs/buildroot/fs/ext_pkg/usr/init_wifi.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-insmod /root/wlan.ko
-ifconfig wlan0 up
-chmod +x /usr/sbin/*
-chmod +x /usr/bin/*
-wpa_supplicant -iwlan0 -c /usr/etc/wpa_supplicant.conf &
-dhcpcd wlan0 &
