98e88c53a7b170b00b8d8e6266bd4d7fc430f68f
kjlee <kjlee@nexell.co.kr>
2016-10-05 20:17:24 +0900

    [PATCH] Corona  - modify mdev
    Signed-off-by: kjlee <kjlee@nexell.co.kr>
    
    Former-commit-id: 222b0795b0a664842df6e8a5f159d01a9c6358f4

--- fs/buildroot/fs/ext_pkg/etc/mdev/automount.sh
+++ fs/buildroot/fs/ext_pkg/etc/mdev/automount.sh
@@ -2,6 +2,7 @@
 
 MNTPATH=$1
 LOGPATH=/dev/console
+CMDLINE=`cat /proc/cmdline | grep -c update`
 
 if [ "$1" == "" ]; then
     echo "automount.sh parameter is none" > $LOGPATH
@@ -12,20 +13,49 @@ mounted=`mount | grep $MNTPATH`
 MNTPATH=$2
 
 if [ "$mounted" = "" ]; then
+	
     echo "Mount : $1 -> $MNTPATH" > $LOGPATH
 	mkdir -p $MNTPATH
-    mount /dev/$1 $MNTPATH
-    wait
-	chown -R default.default /mnt/mmc
-    echo "Done  : mount $MNTPATH" > $LOGPATH
-    if [ "$MNTPATH" == "/mnt/mmc" ]; then
-        if [ -f /mnt/mmc/init.sh ]; then
-            echo ""
-        else
-            busybox cp /usr/etc/.init.sh /mnt/mmc/init.sh
+
+	if [ ${CMDLINE} == "1" ]; then
+	    if [ "$MNTPATH" == "/mnt/mmc" ]; then
+			echo ""
+		else
+		    mount /dev/$1 $MNTPATH
 		fi
-        /mnt/mmc/init.sh
-    fi
+	else
+	    mount /dev/$1 $MNTPATH
+	fi
+
+    wait
+
+	if [ ${CMDLINE} == "1" ]; then
+	    if [ "$MNTPATH" == "/mnt/mmc" ]; then
+			echo ""
+		else
+		    echo "Done  : mount $MNTPATH" > $LOGPATH
+		    if [ "$MNTPATH" == "/mnt/mmc" ]; then
+				chown -R default.default /mnt/mmc
+		        if [ -f /mnt/mmc/init.sh ]; then
+		            echo ""
+		        else
+		            busybox cp /usr/etc/.init.sh /mnt/mmc/init.sh
+				fi
+		        /mnt/mmc/init.sh
+		    fi
+		fi
+	else
+		echo "Done  : mount $MNTPATH" > $LOGPATH
+        if [ "$MNTPATH" == "/mnt/mmc" ]; then
+	        chown -R default.default /mnt/mmc
+	        if [ -f /mnt/mmc/init.sh ]; then
+		        echo ""
+            else
+	            busybox cp /usr/etc/.init.sh /mnt/mmc/init.sh
+            fi
+            /mnt/mmc/init.sh
+	    fi
+	fi
 
 	if [ "$MNTPATH" == "/mnt/usb" ]; then
 		sleep 3
